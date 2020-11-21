dd12e28ed781d2253fe916244af9e1bfb082f5f3
Николай Иртуганов < @DESKTOP-OTOQB2C>
2020-11-21 18:11:26 +0300

    добавлен post запрос

--- JsonParsing/MainWindow.xaml.cs
+++ JsonParsing/MainWindow.xaml.cs
@@ -2,8 +2,10 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http.Headers;
 
 namespace JsonParsing
 {
@@ -23,11 +26,13 @@ namespace JsonParsing
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int ROLE_ID = 1;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            using(var webClient = new WebClient())
+            using (var webClient = new WebClient())
             {
                 // Устанавливаем кодировку для символов кириллицы
                 webClient.Encoding = Encoding.UTF8;
@@ -46,7 +51,40 @@ namespace JsonParsing
                 //Егор - твое дз спарсить вот это
                 // http://api.themoviedb.org/3/movie/upcoming?api_key=fa844ca3beb3b19b4a871a3662894d27
                 // http://vongu3-001-site2.ctempurl.com/api/userinfo
+
+                // post запрос
+                //var pars = new NameValueCollection
+                //{
+                //    { "Email", "fromVS" },
+                //    { "Password", "VS" },
+                //    { "PhoneNumber", "89267090729" },
+                //    { "CompanyName", "SberBank" },
+                //    { "RoleId", "1" }
+                //};
+                //webClient.UploadValues("http://vongu3-001-site2.ctempurl.com/api/Registration","POST", pars);
+                UserInfo user = new UserInfo();
+                user.Email = "VS2@mail.ru";
+                user.Password = "1234";
+                user.PhoneNumber = "89267090729";
+                user.RoleId = 1;
+                user.CompanyName = "MyCompany2";
+                string Serialized = JsonConvert.SerializeObject(user);
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpContent content = new StringContent(Serialized, Encoding.Unicode, "application/json");
+                    var response = client.PostAsync("http://vongu3-001-site2.ctempurl.com/api/Registration", content);
+                    //var status = response.Status;
+                    var result = response.Result;
+                    MessageBox.Show(result.ToString());
+                }
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ JsonParsing/Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonParsing.Models
+{
+    class UserInfo
+    {
+        public String Email;
+        public String Password;
+        public int RoleId;
+        public String PhoneNumber;
+        public String CompanyName;
+    }
+}
